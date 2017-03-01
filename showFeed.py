402173bf22ecac18b666474f1bdb6ec90b95eb2b
themaanas <themaanas@gmail.com>
2017-02-28 19:32:15 -0500

    Add files via upload

--- showFeed.py
+++ showFeed.py
@@ -9,10 +9,11 @@ def show_webcam(mirror=False):
 	EVENT_LIST = 'eventList.csv'
 	inputString = ""
 	cam = cv2.VideoCapture(0)
+	iAr = []
+	file = "test_image.png"
 	while True:
 		ret_val, img = cam.read()
 		camera_capture = img
-		file = "test_image.png"
 		if mirror: 
 			img = cv2.flip(img, 1)
 		cv2.imshow('1089 Scouting Scanner', img)
@@ -20,29 +21,31 @@ def show_webcam(mirror=False):
 			break  # esc to quit
 		cv2.imwrite(file, camera_capture)
 		qr = qrtools.QR()
-		qr.decode("test_image.png") 
+		qr.decode(file) 
 		if qr.data != "NULL" and qr.data != tempString:
 			inputString = qr.data
 			iAr = inputString.strip().split(",")
-			chunks = lambda iAr, n=:10 [iAr[i:i+n] for i in range(0, len(iAr), n)]
-			with open(SETUP_LIST, 'a+') as csvfile:
+			team=iAr[2]
+			match=iAr[1]
+			chunks = lambda iAr, n=10: [iAr[i:i+n] for i in range(0, len(iAr), n)]
+			with open(SETUP_LIST, 'ab+') as csvfile:
 				csvWrite = csv.writer(csvfile, dialect='excel', delimiter=',')
 #				print chunks(iAr)
 #				print len(iAr)
-				csvWrite.writerow(iAr[:6] + iAr[len(iAr)-7:])
-			with open(EVENT_LIST, 'a+') as csvfile:
+				csvWrite.writerow(iAr[:7] + iAr[len(iAr)-7:])
+			with open(EVENT_LIST, 'ab+') as csvfile:
 				csvWrite = csv.writer(csvfile, dialect='excel', delimiter=',')
-				setupArr = [iAr[2],iAr[1]]
-				newEvent = iAr
-				del newEvent[:6]
+				setupArr = [team,match]
+				newEvent = iAr	
+				del newEvent[:7]
 				del newEvent[len(newEvent)-7:]
-				print len(chunks(newEvent))
-				for i in chunks(newEvent):
-					csvWrite.writerow(setupArr + i)
+				chunklen = len(chunks(newEvent))
+				for i in chunks(newEvent)[:chunklen-1]:
+					csvWrite.writerow(i)
 			tempString = qr.data
+			print "Saved"
 		else:
 			camera_capture = img
-			file = "test_image.png"
 	cv2.destroyAllWindows()
 
 def main():
